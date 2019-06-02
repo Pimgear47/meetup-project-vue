c10b50de490e8caf6cb032e40119f45fce2faddf
Kunjanaphorn <Kunjanaphorn.b@gmail.com>
2019-06-03 01:27:59 +0700

    Adding a Create Meetup Form

--- src/store/index.js
+++ src/store/index.js
@@ -29,8 +29,25 @@ export const store = new Vuex.Store({
             registeredMeetups: ['asdsadsadff1ssd']
         }
     },
-    mutations: {},
-    actions: {},
+    mutations: {
+        createMeetup(state, payload) {
+            state.loadedMeetups.push(payload)
+        }
+    },
+    actions: {
+        createMeetup({ commit }, payload) {
+            const meetup = {
+                    title: payload.title,
+                    location: payload.location,
+                    imageUrl: payload.imageUrl,
+                    description: payload.description,
+                    date: payload.date,
+                    id: 'sadasfkfosafofasip'
+                }
+                // Reach out to firebase and store it
+            commit('createMeetup', meetup)
+        }
+    },
     getters: {
         loadedMeetups(state) {
             return state.loadedMeetups.sort((meetupA, meetupB) => {
